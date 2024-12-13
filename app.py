32fa2374f1a8e3935bb90f2bfb9cfcbbd2b9f44a
Carlos Daniel Oliveira <carlosdanielbarbosadeoliveira@gmail.com>
2024-12-12 22:42:43 -0300

    tipos de clicks

--- app.py
+++ app.py
@@ -1,19 +1,60 @@
 import pyautogui
 
+
 from time import sleep
 
 
 '''
+
 from  mouseinfo import mouseInfo
 mouseInfo()
 
 '''
 
-#Posição de algo
 
-pyautogui.moveTo(576,571,duration=1)
-for i in range(0,100):
+
+#Posição e clicando em algo
+
+'''pyautogui.moveTo(576,571,duration=1)
+for i in range(0,1):
     sleep(0.1)
     pyautogui.click()
 
-#pyautogui.move(1900,0,duration=2)
+#pyautogui.move(1900,0,duration=2)'''
+
+
+
+
+
+'''
+#click personalizado
+
+pyautogui.click (
+    x = 1000,
+    y = 600,
+    clicks= 3,
+    interval= 0.1,
+    button= 'left',
+    duration= 0.1
+
+)
+
+# clicando na posição atual 
+
+pyautogui.click()
+pyautogui.click(button='left')
+pyautogui.click(button='right')
+pyautogui.click(button='middle')
+
+
+#clicar x vezes 
+
+pyautogui.click(clicks=2)
+
+# funções prontas para clicks
+
+pyautogui.leftClick()
+pyautogui.rightClick()
+pyautogui.middleClick()
+pyautogui.tripleClick()
+'''
